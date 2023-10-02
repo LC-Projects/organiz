08f349d8d0f3c2945ba44bd7e8eba3c47532dd98
Lucky Marty <contact@luckymarty.fr>
2023-10-02 14:59:07 +0200

    🎨 THEME Constants

--- constants/routes.js
+++ constants/routes.js
@@ -1,4 +1,4 @@
 export default {
     LOGIN: 'login',
-    BOARD: 'site',
+    BOARD: 'Board',
 }
--- views/board/Board.jsx
+++ views/board/Board.jsx
@@ -1,15 +1,48 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
+import { COLORS, THEME } from "../../constants";
 
 const Board = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Todo</Text>
+          <Text style={styles.headerCounte}>4</Text>
+        </View>
+
+        <View style={styles.tasks}>
+
+        </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 
 export default Board;
+
+const styles = StyleSheet.create({
+  container: {},
+  header: {
+    flexDirection: "row",
+    margin: THEME.spacing.s,
+    padding: THEME.spacing.s,
+    backgroundColor: COLORS.white,
+    borderRadius: THEME.border.m
+  },
+  headerTitle: {
+    flex: 1,
+    fontSize: THEME.font.size.xl,
+    fontWeight: THEME.font.weight.bold,
+  },
+  headerCounte: {
+    flex: 1,
+    textAlign: 'right',
+    fontSize: THEME.font.size.xl,
+    fontWeight: THEME.font.weight.bold,
+  },
+
+  tasks: {
+
+  }
+});
