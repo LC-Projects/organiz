8ea48f531ce7af556df89698dd0a945886832514
Lucky Marty <contact@luckymarty.fr>
2023-10-03 11:41:38 +0200

    ✨ Add DATABASE

--- api/firebase/connect.jsx
+++ api/firebase/connect.jsx
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// import { getDatabase } from "firebase/database";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAbJ8m3dRm788o9Hv0L845MJdWfGioYj9c",
@@ -9,10 +9,11 @@ const firebaseConfig = {
   projectId: "organiz-f07be",
   storageBucket: "organiz-f07be.appspot.com",
   messagingSenderId: "708456224457",
-  appId: "1:708456224457:web:931d60afbd6c886456df18"
+  appId: "1:708456224457:web:931d60afbd6c886456df18",
+  databaseURL: "https://organiz-f07be-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-// export const FIREBASE_DBRT = getDatabase(FIREBASE_APP);
+export const FIREBASE_DBRT = getDatabase(FIREBASE_APP);
