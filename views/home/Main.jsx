315c7afb4b89522702a63356398e9ca1189501c2
Lucky Marty <contact@luckymarty.fr>
2023-10-03 16:37:07 +0200

    ✨ Bottom Nav: Icons

--- views/home/Main.jsx
+++ views/home/Main.jsx
@@ -3,25 +3,63 @@ import { View, Text, StyleSheet } from "react-native";
 import Homepage from "./Homepage";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { COLORS } from "../../constants";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import AddBoard from "./AddBoard";
 
 const Tab = createBottomTabNavigator();
 
 const Main = () => {
+
+    function handleAddBoard(event) {
+        event.preventDefault();
+        alert("OMG!")
+    }
+
+
   return (
     <Tab.Navigator
       screenOptions={{
         tabBarStyle: styles.bottomBar,
+        tabBarActiveTintColor: "#fff",
       }}
     >
       <Tab.Screen
         name="Home"
         component={Homepage}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <Icon name="home" size={25} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="EmptyComponent" //Render Empty Component
+        component={AddBoard}
+        options={{
+          headerShown: false,
+          tabBarLabel: "",
+          tabBarIcon: ({ color }) => (
+            <View style={{marginTop: '-10%', backgroundColor: COLORS.dark_gray, borderRadius: 50, padding: 4}}>
+                <View style={{width: 50, height: 50,backgroundColor: COLORS.dark_gray, borderRadius: 50, borderWidth: 2, borderColor: COLORS.white}}>
+                    <Icon name="add" size={44} color={color} style={{textAlign: 'center', color: COLORS.white}} />
+                </View>
+            </View>
+          ),
+        }}
+        listeners={() => ({tabPress: event => handleAddBoard(event)})}
       />
       <Tab.Screen
         name="Profile"
         component={Homepage}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <Icon name="person" size={25} color={color} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
@@ -40,6 +78,6 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.75,
     elevation: 10,
-    borderWidth: 0
+    borderWidth: 0,
   },
 });
