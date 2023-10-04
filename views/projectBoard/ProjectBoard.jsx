380f1d8aa16ca940462d9f711e00fef0dbeeaceb
Lucky Marty <contact@luckymarty.fr>
2023-10-04 15:46:06 +0200

    ♻️ ProjectBoards

--- views/projectBoard/ProjectBoard.jsx
+++ views/projectBoard/ProjectBoard.jsx
@@ -1,11 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import {
-  Alert,
-  StyleSheet,
-  Text,
-  View,
-  useWindowDimensions,
-} from "react-native";
+import { Alert, StyleSheet, useWindowDimensions } from "react-native";
 import { SceneMap, TabBar, TabView } from "react-native-tab-view";
 import Board from "../board/Board";
 import { COLORS, THEME } from "../../constants";
@@ -14,11 +8,54 @@ import { userContext } from "../../context/userContext";
 import { appContext } from "../../context/appContext";
 
 const ProjectBoard = ({ route }) => {
+  // Context
   const { user, setUser } = useContext(userContext);
   const { refresh, setRefresh } = useContext(appContext);
+
+
+  // Inititalization
+  const layout = useWindowDimensions();
+  const titles = [
+    { name: "To Do", key: "todo" },
+    { name: "Doing", key: "doing" },
+    { name: "Done", key: "done" },
+  ];
   const [boardId, setstate] = useState("");
   const [data, setData] = useState(null);
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: titles[0].key, title: titles[0].name },
+    { key: titles[1].key, title: titles[1].name },
+    { key: titles[2].key, title: titles[2].name },
+  ]);
+  const renderScene = SceneMap({
+    todo: () => (
+      <Board title={titles[0].name} data={data?.todo} keyName={titles[0].key} />
+    ),
+    doing: () => (
+      <Board
+        title={titles[1].name}
+        data={data?.doing}
+        keyName={titles[1].key}
+      />
+    ),
+    done: () => (
+      <Board title={titles[2].name} data={data?.done} keyName={titles[2].key} />
+    ),
+  });
 
+
+  // Function
+  const renderTabBar = (props) => (
+    <TabBar
+      {...props}
+      indicatorStyle={styles.tabBarIndicator}
+      style={styles.tabBarContainer}
+    />
+  );
+
+
+  // Hook
   useEffect(() => {
     setstate(route.params?.boardId);
     (async () => {
@@ -33,35 +70,8 @@ const ProjectBoard = ({ route }) => {
     })();
   }, [boardId, refresh]);
 
-  const titles = [
-    { name: "To Do", key: "todo" },
-    { name: "Doing", key: "doing" },
-    { name: "Done", key: "done" },
-  ];
 
-  const renderScene = SceneMap({
-    todo: () => <Board title={titles[0].name} data={data?.todo} keyName={titles[0].key} />,
-    doing: () => <Board title={titles[1].name} data={data?.doing} keyName={titles[1].key} />,
-    done: () => <Board title={titles[2].name} data={data?.done} keyName={titles[2].key} />,
-  });
-
-  const layout = useWindowDimensions();
-
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: titles[0].key, title: titles[0].name },
-    { key: titles[1].key, title: titles[1].name },
-    { key: titles[2].key, title: titles[2].name },
-  ]);
-
-  const renderTabBar = (props) => (
-    <TabBar
-      {...props}
-      indicatorStyle={styles.tabBarIndicator}
-      style={styles.tabBarContainer}
-    />
-  );
-
+  // Render
   return (
     <TabView
       renderTabBar={renderTabBar}
@@ -76,6 +86,8 @@ const ProjectBoard = ({ route }) => {
 
 export default ProjectBoard;
 
+
+// Style
 const styles = StyleSheet.create({
   container: {
     backgroundColor: COLORS.dark_gray,
