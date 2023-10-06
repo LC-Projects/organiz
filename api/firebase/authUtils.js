a881e9d1287290a9f6a21a056cdd19b1b6821d69
Lucky Marty <contact@luckymarty.fr>
2023-10-06 19:35:22 +0200

    ✨ Send Verification Email

--- api/firebase/authUtils.js
+++ api/firebase/authUtils.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithCredential, signInWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, sendEmailVerification, signInWithCredential, signInWithEmailAndPassword } from "firebase/auth";
 import { FIREBASE_AUTH } from "./connect";
 
 const auth = FIREBASE_AUTH;
@@ -6,8 +6,14 @@ const auth = FIREBASE_AUTH;
 export async function createUser(email, password) {
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-        const user = userCredential.user;
-        return user
+            .then(() => {
+                sendEmailVerification(auth.currentUser, {
+                    handleCodeInApp: true,
+                    url: "https://organiz-f07be.firebaseapp.com",
+                })
+        })
+        // const user = userCredential.user;
+        return "Please check your email!"
     }
     catch (error) {
         const errorCode = error.code;
